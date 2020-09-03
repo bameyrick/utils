934965161f4d568a6d8a449df09895b2ad60ebd1
Ben Meyrick <bameyrick@gmail.com>
2020-09-03 16:01:13 +0100

    Fix codact issues

--- tests/isEmpty.spec.ts
+++ tests/isEmpty.spec.ts
@@ -1,31 +1,31 @@
 import { isEmpty } from '../src';
 
-describe(`isEmpty`, () => {
-  it(`should return true if value is ''`, () => {
+describe('isEmpty', () => {
+  it('should return true if value is ""', () => {
     const value = '';
 
     expect(isEmpty(value)).toBe(true);
   });
 
-  it(`should return true if value is '   '`, () => {
+  it('should return true if value is ""', () => {
     const value = '   ';
 
     expect(isEmpty(value)).toBe(true);
   });
 
-  it(`should return false if value is a non empty string`, () => {
+  it('should return false if value is a non empty string', () => {
     const value = 'yes';
 
     expect(isEmpty(value)).toBe(false);
   });
 
-  it(`should return true if value is undefined`, () => {
+  it('should return true if value is undefined', () => {
     const value = undefined;
 
     expect(isEmpty(value)).toBe(true);
   });
 
-  it(`should return true if value is not provided`, () => {
+  it('should return true if value is not provided', () => {
     // tslint:disable-next-line: prefer-const
     let value: string;
 
--- tests/isNullOrUndefined.spec.ts
+++ tests/isNullOrUndefined.spec.ts
@@ -1,37 +1,37 @@
 import { isNullOrUndefined } from '../src';
 
-describe(`isNullOrUndefined`, () => {
-  it(`should return true if value is null`, () => {
+describe('isNullOrUndefined', () => {
+  it('should return true if value is null', () => {
     const value = null;
 
     expect(isNullOrUndefined(value)).toBe(true);
   });
 
-  it(`should return true if value is undefined`, () => {
+  it('should return true if value is undefined', () => {
     const value = undefined;
 
     expect(isNullOrUndefined(value)).toBe(true);
   });
 
-  it(`should return false if value is 'undefined'`, () => {
+  it('should return false if value is "undefined"', () => {
     const value = 'undefined';
 
     expect(isNullOrUndefined(value)).toBe(false);
   });
 
-  it(`should return false if value is empty string'`, () => {
+  it('should return false if value is empty string', () => {
     const value = '';
 
     expect(isNullOrUndefined(value)).toBe(false);
   });
 
-  it(`should return false if value is 0'`, () => {
+  it('should return false if value is 0', () => {
     const value = 0;
 
     expect(isNullOrUndefined(value)).toBe(false);
   });
 
-  it(`should return true if value is not set'`, () => {
+  it('should return true if value is not set', () => {
     // tslint:disable-next-line: prefer-const
     let value: void;
 
