f478688f6d503a7e0d633aed62884eaf81712e91
Ben Meyrick <bameyrick@gmail.com>
2021-06-04 14:47:44 +0100

    Fix return type for isEmpty when provided value is null or undefined (#24)
    * Fix return type for isEmpty when provided value is null or undefined
    
    * Add test to cover line 41 of isEmpty

--- src/isEmpty.ts
+++ src/isEmpty.ts
@@ -16,27 +16,29 @@ type EmptyResult<T> = T extends string
   ? Empty.Array
   : T extends object
   ? Empty.Object
-  : T extends null
-  ? null
-  : T extends undefined
-  ? undefined
   : never;
 
 /**
  * Checks if a given value is empty
  */
 // tslint:disable-next-line: no-any
-export function isEmpty<T extends string | any[] | object>(value: T | Empty | null | undefined): value is EmptyResult<T> {
+export function isEmpty<T extends string | any[] | object | null | undefined>(
+  value: T | Empty | null | undefined
+): value is EmptyResult<T> | null | undefined {
   if (isNullOrUndefined(value)) {
     return true;
   }
 
   switch (typeof value) {
     case 'object': {
-      return !Object.keys(value).length;
+      // tslint:disable-next-line: no-any
+      return !Object.keys(value as any).length;
     }
     case 'string': {
       return !value.trim();
+    }
+    default: {
+      return false;
     }
   }
 }
