9cce2c64baa23e83b079b674ea2f5419c5a269fc
Ben Meyrick <bameyrick@gmail.com>
2022-09-29 17:39:14 +0100

    Fix randomNumberBetweenRange returning a number one less than min or mone more than max

--- src/randomNumberBetweenRange.ts
+++ src/randomNumberBetweenRange.ts
@@ -2,5 +2,10 @@
  * Returns a random whole number between a given range
  */
 export function randomNumberBetweenRange(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  const value = Math.random() * (max - min + 1) + min;
+
+  return Math.floor(value);
 }
